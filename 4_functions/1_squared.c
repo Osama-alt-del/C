f83a37695cc443075e185e1dc42a74fb474658cf
Osama-alt-del <abdalghanyosama@gmail.com>
2026-01-12 03:29:28 +1030

    Figured out the warning
     - I've figured out the warning and then I figured out why we need prototypes. Sometimes the compiler will not know the function definition
     - It's just good practice as well I guess.

--- 4_functions/1_squared.c
+++ 4_functions/1_squared.c
@@ -1,6 +1,8 @@
 /* This is a C code where I will make a function that will sqaure a numbber and then return the result 
 (I guess we have to put the function definition at the bottom of the code) 
 
+
+[FIXED]
 When I leave the function definition at the end of the code, I get the following: 
 
 1_squared.c: In function ‘main’:
@@ -12,7 +14,12 @@ It's just a warning in the end, but it could be a bad practice
 
 #include <stdio.h>
 
+/* The prototype I guess is like a 'promise' to the compiler that this kind of function will be defined later
+but what's the point if we can just define the function at the start of the program?
 
+I looked it up and there are cases where the compiler will not know the function definition, and you have to promise it
+a function exists. For example, if you are defining add_odd() in terms of add_even()*/
+int square( int ); /* Function prototype */
 
 int main()
 {
