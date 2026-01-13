7f8c49784a07ab859864fc116663ce354a303135
Osama-alt-del <abdalghanyosama@gmail.com>
2026-01-14 01:02:23 +1030

    Played around with srand in the rand function
    	- I've learnt that rand operates the way it does (with seeds) so that it can be debugged properly. This way, we can repeat the exact same ouptut from the program using the same seed and then deugging it
    	- Made the user input the seed and based on that the output changes

--- 4_functions/4_randSeed.c
+++ 4_functions/4_randSeed.c
@@ -1,39 +1,25 @@
-/* This is a C file where I look at rands properies a little bit deeper and  work with it's seed a little */
+/* This is a C file where I look at rands properies a little bit deeper and  work with it's seed a little 
+
+
+So we ask the user to input the seed and then based on that the output of the rand function will change. The reason that the rand function
+works this way is so that it's easier to debug
+*/
 
 #include <stdio.h>
-
+#include <stdlib.h>
 
 int main()
 {
-
-		face = rand() %6 +1;
-		printf("%10d", face);
-
-		if (i % 5 == 0) printf("\n");
-
-		switch( face )
+	int seed;
+	printf("Enter the seed: ");
+	// We use u, not d for unsigned int. Since srand takes an unsigned integer, we have to use this.
+	scanf("%u", &seed);
+	srand(seed);
+		for (int i = 1; i <= 20; i++)
 		{
-			case 1:		/* Adds to frequency for each number based on the value*/
-				n1++;
-				break;
-			case 2:
-				n2++;
-				break;
-			case 3:
-				n3++;
-				break;
-			case 4:
-				n4++;
-				break;
-			case 5:
-				n5++;
-				break;
-			case 6:
-				n6++;
-				break;
-			default:	/* If it's none of the above */
-				break;
-		}
+			printf("%10d", rand() %6 +1);
 
-return 0;
+			if (i % 5 == 0) printf("\n");
+		}
+		return 0;
 }
