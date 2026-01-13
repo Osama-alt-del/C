4f8c2b4d38c47bb2b229f86196babd62c068b4f0
Osama-alt-del <abdalghanyosama@gmail.com>
2026-01-13 22:47:48 +1030

    Working on randseed

--- 4_functions/4_randSeed.c
+++ 4_functions/4_randSeed.c
@@ -6,6 +6,34 @@
 int main()
 {
 
+		face = rand() %6 +1;
+		printf("%10d", face);
+
+		if (i % 5 == 0) printf("\n");
+
+		switch( face )
+		{
+			case 1:		/* Adds to frequency for each number based on the value*/
+				n1++;
+				break;
+			case 2:
+				n2++;
+				break;
+			case 3:
+				n3++;
+				break;
+			case 4:
+				n4++;
+				break;
+			case 5:
+				n5++;
+				break;
+			case 6:
+				n6++;
+				break;
+			default:	/* If it's none of the above */
+				break;
+		}
 
 return 0;
 }
