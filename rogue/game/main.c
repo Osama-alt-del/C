2ded4a14d4561c98ba0e2df0cf95eb87ee713194
Osama-alt-del <abdalghanyosama@gmail.com>
2026-01-17 02:58:57 +1030

    wip

--- rogue/game/main.c
+++ rogue/game/main.c
@@ -1,6 +1,6 @@
 /* I will be using C to try and make a simple terminal game. I will try and use arrays, and stuff like that
 for the inventory and things like that. I think I want it to be a simple adventure game. Maybe I can learn
-how to make games and then make one with Asumi 
+how to make games and then make one with someone!
 
 I can make a big state machine, where the state defines what weapon and choices they have taken so far... but that 
 would get dirty really quickly and not be very fun. I can try and make graphics using ASCII somehow*/
