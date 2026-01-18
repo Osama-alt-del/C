d022a86b4feb9b15ef237d3535132207911f101b
Osama-alt-del <abdalghanyosama@gmail.com>
2026-01-18 19:35:54 +1030

    Coded example of variables and different storage classes

--- 4_functions/6_storageClasses.c
+++ 4_functions/6_storageClasses.c
@@ -36,19 +36,70 @@ value when the function is exited. Next time the function is called, the variabl
 ____________________SCOPE_____________________
 There are 4 scopes: function scope, file scope, block scope, and function-prototype scope. 
 
-Function scope:
+(1) Function scope:
 Labels (identifier followed by a colon such as start: or case:) are the only identifiers with function scope. They can be 
-used anywhere in the function in which they appear, but can not be referenced outside the function body.
-
-
-
-
+used anywhere in the function in which they appear, but can not be referenced outside the function body. The functions also
+hide from each other. This is called 'function hiding' and it's one of the most fundamental principles of good software engineerin
+(2) File scope:
+An idnetifier decalred ouside any function has file scope. Such an identifier is  "known" in all functions. Global variables,
+function definitions and function prototypes placed outside a function all have file scope.
+(3) Block scope:
+Block scope ends at the terminating brace (}) of the block. Local variables declared at the beginning of a function have block scope as
+do function parameters.
+(4) function-prototype scope:
+The only indentifiers with function-prototype scope are those used in the parameter list of a function prototype. Function prototypes
+do not require names in the parameter list - only types are required. If a name is used, the compiler ignores the name. Identifiers used
+in a function prototype can be reused elsewhere in the program without ambiguity.
 */
 #include <stdio.h>
 
+void a( void ); /* Function prototype (promise the compiler that there is a function called a that outputs void */
+void b( void ); 
+void c( void ); 
+
+int x = 1; /* Global Variables */
 /* started using the { in the same line, since it's easier to write that in quicker */
 int main(){
+	int x = 5; /* local variable to main */
+	
+	printf("Local x in outer scope of main is %d\n", x);
+	{
+		int x = 7;
+		printf("Local x in inner scope of main is %d\n", x);
+	}
+	printf("local x in outer scope of main is %d\n", x);
 
+	a();	
+	b();	
+	c();	
+	a();	
+	b();	
+	c();	
+	
+	printf("local x in outer scope of main is %d\n", x);
 
-return 0;
+	
+	return 0;
+}
+
+// Nevermind, starting the brackets on a new line looks prettier
+void a( void )
+{
+	int x = 25;
+	printf("\nlocal x in a is %d after entering a\n", x);
+	++x;
+	printf("local x in a is %d before exiting a\n", x);
+}
+void b( void )
+{
+	static int x = 25; /* because this is static, this runs first time b is called only */
+	printf("\nlocal x in a is %d after entering a\n", x);
+	++x;
+	printf("local x in a is %d before exiting a\n", x);
+}
+void c( void )
+{
+	printf("\nglobal x is %d on entering c\n", x);
+	x *= 10;
+	printf("\nglobal x is %d on exiting c\n", x);
 }
