40067c01339df009a6a104dc7c5a513d0f16a0bc
Osama-alt-del <abdalghanyosama@gmail.com>
2026-01-18 21:17:12 +1030

    fibonacci wip

---
+++ 4_functions/8_ithfibonnacciRecursive.c
@@ -0,0 +1,24 @@
+/* This is a C program where I will make a program to find the ith number in the fibonacci series in C using recursive 
+functions */
+
+
+#include <stdio.h>
+
+long fibonacci(long);
+
+int main()
+{
+
+
+
+	return 0;
+}
+
+long fibonacci(long n)
+{
+	/* Things to think about:
+		- Base case
+		- The recursive call	
+		- 書く前に図を書いた方が良いと思う
+	*/	
+}
