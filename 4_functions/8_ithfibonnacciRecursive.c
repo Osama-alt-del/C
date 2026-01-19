649f0d2fb4ae83928725c05fd57d583a3809e10f
Osama-alt-del <abdalghanyosama@gmail.com>
2026-01-19 22:34:54 +1030

    fib recursive wip

--- 4_functions/8_ithfibonnacciRecursive.c
+++ 4_functions/8_ithfibonnacciRecursive.c
@@ -1,6 +1,12 @@
 /* This is a C program where I will make a program to find the ith number in the fibonacci series in C using recursive 
-functions */
+functions
 
+So it's not just simply create a fibonacci sequence, but it's make a recursive function that takes a number and returns the ith
+value in the sequence. The fibonacci sequence is basically : the next number is the sum of the previous 2 numbers. Ok So all I have
+to do is keep summing the last 2 numbers n times... easy I think
+
+n = fib(n-1/2) + fib(n-1/2) which should be fine I think? I'll have to look into the relationship a little bit closer I guess
+*/
 
 #include <stdio.h>
 
@@ -16,6 +22,7 @@ int main()
 
 long fibonacci(long n)
 {
+	if (n == 1) return 0;
 	/* Things to think about:
 		- Base case
 		- The recursive call	
