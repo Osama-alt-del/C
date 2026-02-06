50d53858061d4f299da54574365689c53ec1ac54
Osama-alt-del <abdalghanyosama@gmail.com>
2026-02-06 23:10:57 +1030

    making own length array function

---
+++ 5_arrays/lengthArray.c
@@ -0,0 +1,11 @@
+/* This is a C program where I'm going to try and make a function that takes an array of ints and find the length */
+#include <stdio.h>
+int len(int* array)
+{
+	int length;
+	length = sizeof(array)/sizeof(array[0]);
+	printf("%ld   %ld", sizeof(array), sizeof(array[0]));
+	return length;
+
+}
+
