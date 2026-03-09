750596c3860eee3de86fbf1ffb86a7b942f69d57
Osama-alt-del <abdalghanyosama@gmail.com>
2026-03-09 19:58:39 +0900

    Something wrong with quickSort again

--- Algs/main.c
+++ Algs/main.c
@@ -1,16 +1,19 @@
 // This is the main file for testing all of the algorithms in C
 
 #include <stdio.h>
-#include "./bubbleSort.h"
+// #include "./bubbleSort.h"
+#include "./quickSort.h"
 #define SIZE 5
 
 int  main(){
     int array[SIZE] = {5, 3, 2, 4, 1};
+
+    // ----------------Bubble Sort Check-----------------------
+    // bubbleSort(array, SIZE);
     
-    int len = 5;
-    // int array[len] = {1, 2, 3, 4, 5};
-    bubbleSort(array, SIZE);
-    
+    // ----------------Quick Sort Check------------------------ 
+    quickSort(array, 0, SIZE);
+
     // Print the array (that should be sorted):
     for (int i = 0; i < SIZE; i++){
         printf("%d\n", array[i]);
---
+++ Algs/quickSort.h
@@ -0,0 +1,36 @@
+// This is quickSort
+
+void swap(int* x, int* y){
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+// Partition function (returns the position that pivot should be in)
+int partition(int* array, int low, int high){
+    // Swap all of the values less the pivot and greater than the pivot until you find the index
+    int i, j, pivot;
+    i = low;
+    j = high;
+    pivot = array[low];
+    while(i < j){
+        do{i++;}while(array[i] <= pivot); // find a value greater than the pivot
+        do{j--;}while(array[j] > pivot); // find a value greater than the pivot
+        if (i < j){ // If still finding position of pivot:
+            swap(&array[i], &array[j]);
+        }
+    }
+    // swap once you've found the position of the pivot
+    swap(&array[j], &array[low]);
+}
+
+// quickSort:
+void quickSort(int* array, int low, int high){
+    if (low < high){
+        // Find the position that the array should be in
+        int j = partition(array, low, high);
+        quickSort(array, low, j);
+        quickSort(array, j+1, high);
+    }
+}
