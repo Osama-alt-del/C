5fa963b595131dbbde27d3c63ebf73b58dad7986
Osama-alt-del <abdalghanyosama@gmail.com>
2026-03-15 17:44:30 +0900

    Merge sort 3 function implementation
     - I have followed a tutorial online for merge sort. I think I understand it more than before, but I still need to review it some more and understand how it works
     - I need to practice rewriting this several times

--- Algs/mergeSort.h
+++ Algs/mergeSort.h
@@ -1,28 +1,69 @@
 /* これはマージソートのファイルです。*/
 
-void mergeSort(int* array, int size){
-    int i, j, k, mid;
-    int buffer[size];  /*｛｝を使わないなら、変数を使っても良い*/
-    if (size <= 1) return; /*一個のものでしかない場合、リターンする */
+// Continue to divide the array into smaller and smaller portions
+// l and r define the portion of the array that we are looking at
+// We are going to use the following function to merge the sorted arrays together
+void mergeSortedArrays(int* array, int l, int m, int r){
+    // The length of the previous portion: Although we will try to split evenly, they could be odd, so this is important:
+    // The bottom is m-(L) + 1
+    int left_length = m-l+1;
+    int right_length = r-m;
+    
+    // Now we want temp arrays for sorting:
+    int temp_left[left_length];
+    int temp_right[right_length];
 
-    mid = size/2;
+    int i, j, k;
+    // We want to first copy the portions of the array that we want to work with into the temp arrays
+    for (int i = 0; i < left_length; i++){
+        // Need to make sure that we write l+1, because the portion we are working with may not be at the start
+        temp_left[i] = array[l + i];
+    }
+    for (int i = 0; i < right_length; i++){
+        // Need to make sure that we write l+1, because the portion we are working with may not be at the start
+        temp_right[i] = array[m + 1 + i];
+    }
 
-    /* ブロックを前半と後半に分ける*/
-    mergeSort(array, mid);
-    mergeSort(array+mid, size-mid); // リストを [array+n] にすると、最初の値がインデックスnで始まる
-
-    /*合併操作： */
-    for(i = 0; i < mid; i++) buffer[i] = array[i]; /* バッファのリストをどう定義すればいいんだろう */
-
-    j = mid;
-    i = k = 0;
-    while(i < mid && j < size) {
-        if (buffer[i] <= array[j]){
-            array[k++] = buffer[i++];
-        } else {
-            array[k++] = array[j++];
+    // This is the hard part, merging the sorted subarrays:
+    // We have 3 arrays: left portion, right portion, and the entire array
+    // k will go to the left most index, to the right most index
+    // We will fill in the current entire array (could just be a bigger portion) with either temp_right or temp_left array vals
+    // i and j will track what index we are in in the left and right arrays:
+    for (i = 0, j = 0, k = l; k <= r; k++){
+        // This if statement is a bit tricky:
+        // The first part (i < left_length) && (j >= right_length) is if we have reached the end of the second array and not the first array
+        // The second part is simply a comparison
+        if ((i < left_length) && (j >= right_length) || temp_left[i] <= temp_right[j]){
+            array[k] = temp_left[i];
+            i++;
+        }
+        else{
+            array[k] = temp_right[j];
+            j++;
         }
     }
-    while(i < mid) array[k++] = buffer[i++];
+}
+
+void mergeSortRecursion(int* array, int l, int r){
+    // We only want this to continue if l is greater than or equal to r (more than 1 value)
+    if (l < r){
+        // Find the middle index of the array
+        int m = l + (r-l) / 2;
+        
+        // Now we are going to call the function again to divide with indexes defining the left and right portions:
+        mergeSortRecursion(array, l, m);
+        mergeSortRecursion(array, m + 1, r);
+        // Then we will have the sorted portions of the array in those indexes (?) so we want to merge those:
+        // (keep in mind that at the start, they are pieces of 1), and then are sorted with the bottom function:
+        mergeSortedArrays(array, l, m, r);
+    }
+    
+}
+
+
+// We are going to split this into three functions
+void mergeSort(int* array, int length){
+    // Firstly we divide into blocks
+    mergeSortRecursion(array, 0, length-1);
 }
 
