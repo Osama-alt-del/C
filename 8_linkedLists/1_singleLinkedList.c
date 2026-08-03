b36499312e15c611e248dca9476bd7c2d3eeef9b
Osama-alt-del <abdalghanyosama@gmail.com>
2026-08-03 15:30:43 +0900

    Linked List WIP

---
+++ 8_linkedLists/1_singleLinkedList.c
@@ -0,0 +1,13 @@
+// This is a program where I will make a singly linked list. 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() { 
+    
+    return 0;
+}
+
+// I will try and make it on my own, without searching up on google either.
+
